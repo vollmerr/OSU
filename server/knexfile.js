4afbab014768cb17fdb5c98538052e24820a6a6b
vollmerr <vollmerr@oregonstate.edu>
2018-01-12 02:15:27 -0800

    fix knexfile db name

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,5 +1,5 @@
 const url = process.env.NODE_ENV === 'development' ?
-  'localhost' :
+  'osu' :
   'mysql://bec70745933b8f:888e8e74@us-cdbr-iron-east-05.cleardb.net/heroku_13d5a4c30c88dc5?reconnect=true';
 
 module.exports = {
