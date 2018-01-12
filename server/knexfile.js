a2e844cceba11ab9e6497381911d3cc2cd35eacc
vollmerr <vollmerr@oregonstate.edu>
2018-01-12 01:53:31 -0800

    why you hate me knex...

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,8 +1,12 @@
+const url = process.env.NODE_ENV === 'development' ?
+  'localhost' :
+  'mysql://bec70745933b8f:888e8e74@us-cdbr-iron-east-05.cleardb.net/heroku_13d5a4c30c88dc5?reconnect=true';
+
 module.exports = {
-    client: 'mysql',
-    connection: {
-      user: 'root',
-      password: 'password',
-      database: 'mysql://bec70745933b8f:888e8e74@us-cdbr-iron-east-05.cleardb.net/heroku_13d5a4c30c88dc5?reconnect=true',
-    },
-  };
+  client: 'mysql',
+  connection: {
+    user: 'root',
+    password: 'password',
+    database: url,
+  },
+};
