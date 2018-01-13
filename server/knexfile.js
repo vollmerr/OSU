bb3f8d2d6e09758a7d24e86e8fa82de284016868
vollmerr <vollmerr@oregonstate.edu>
2018-01-12 19:25:21 -0800

    change knex connection

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,12 +1,22 @@
-const url = process.env.NODE_ENV === 'development' ?
-  'osu' :
-  'mysql://bec70745933b8f:888e8e74@us-cdbr-iron-east-05.cleardb.net/heroku_13d5a4c30c88dc5?reconnect=true';
-
-module.exports = {
-  client: 'mysql',
-  connection: {
-    user: 'root',
-    password: 'password',
-    database: url,
+const connections = {
+  development: {
+    client: 'mysql',
+    connection: {
+      user: 'root',
+      password: 'password',
+      host: 'localhost',
+      database: 'osu',
+    }
   },
+  production: {
+    client: 'mysql',
+    connection: {
+      user: 'bec70745933b8f',
+      password: '888e8e74',
+      host: 'us-cdbr-iron-east-05.cleardb.net',
+      database: 'heroku_13d5a4c30c88dc5',
+    }
+  }
 };
+
+module.exports = connections[process.env.NODE_ENV];
