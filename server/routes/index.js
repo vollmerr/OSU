c1a4e6a98c41aa3cc433921b17bf6db648126e61
vollmerr <vollmerr@oregonstate.edu>
2018-01-17 19:00:43 -0800

    remove server views, add compression

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,6 +3,7 @@ const router = require('express').Router();
 
 /* GET - get home page. */
 router.get('*', function(req, res, next) {
+  console.log('in get...')
   res.sendFile(__dirname + '../../client/build/index.html');
 });
 
