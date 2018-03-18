c9f5532cf3f011a5678b7259111bfbc64c303481
vollmerr <vollmerr@oregonstate.edu>
2018-03-17 20:52:04 -0700

    convert to buildStore

--- server/store/index.js
+++ server/store/index.js
@@ -16,41 +16,10 @@ const query = (query) => (
 );
 
 
-// EQUIPMENT
-const equipment = {
-  // create table
-  create: () => (
-    query(`create table equipment(
-        ${C.EQUIPMENT.ID} int primary key auto_increment,
-        ${C.EQUIPMENT.NAME} varchar(255) not null
-    )`)
-  ),
-  // drop table
-  drop: () => (
-    query('drop table if exists equipment')
-  ),
-  // get all
-  get: () => (
-    query('select * from equipment')
-  ),
-  // create a equipment
-  insert: (values) => (
-    query(`insert into equipment (${Object.keys(values).join(', ')}) values ("${Object.values(values).join('", "')}")`)
-  ),
-  // deletes a equipment by id
-  delete: (id) => (
-    query(`delete from equipment where id=${id}`)
-  ),
-  // edits a equipment by id
-  edit: ({ id, ...rest }) => (
-    query(`update equipment set ${Object.keys(rest).map(x => `${x} = '${rest[x]}'`).join(', ')} where ${C.EQUIPMENT.ID}=${id}`)
-  ),
-};
-
 const buildStore = ({ name, structure }) => ({
   // create table
   create: () => (
-    query(structure)
+    query(`create table ${name}(${structure})`)
   ),
   // drop table
   drop: () => (
@@ -74,81 +43,34 @@ const buildStore = ({ name, structure }) => ({
   ),
 });
 
-// // ROLE
-// const role = {
-//   // create table
-//   create: () => (
-//     query(`create table role(
-//       ${C.ROLE.ID} int primary key auto_increment,
-//       ${C.ROLE.NAME} varchar(255) not null
-//     )`)
-//   ),
-//   // drop table
-//   drop: () => (
-//     query('drop table if exists role')
-//   ),
-//   // get all
-//   get: () => (
-//     query('select * from role')
-//   ),
-//   // create a equipment
-//   insert: (values) => (
-//     query(`insert into role (${C.ROLE.NAME}) values ("${values[C.ROLE.NAME]}")`)
-//   ),
-//   // deletes a equipment by id
-//   delete: (id) => (
-//     query(`delete from role where id=${id}`)
-//   ),
-//   // edits a equipment by id
-//   edit: ({ id, ...rest }) => (
-//     query(`update role set ${Object.keys(rest).map(x => `${x} = '${rest[x]}'`).join(', ')} where id=${id}`)
-//   ),
-// };
 
-const role = buildStore({
-  name: 'role',
+const equipment = buildStore({
+  name: 'equipment',
   structure: `
-    create table role(
-      ${C.ROLE.ID} int primary key auto_increment,
-      ${C.ROLE.NAME} varchar(255) not null
-    )
+    ${C.EQUIPMENT.ID} int primary key auto_increment,
+    ${C.EQUIPMENT.NAME} varchar(255) not null
   `,
 });
 
 
-// ADMIN
-const admin = {
-  // create table
-  create: () => (
-    query(`create table admin(
-        ${C.ADMIN.ID} int primary key auto_increment,
-        ${C.ADMIN.FIRST_NAME} varchar(255) not null,
-        ${C.ADMIN.LAST_NAME} varchar(255) not null,
-        ${C.ADMIN.ROLE_ID} int not null,
-        foreign key fk_roleId(${C.ADMIN.ROLE_ID}) references role(${C.ROLE.ID})
-    )`)
-  ),
-  // drop table
-  drop: () => (
-    query('drop table if exists admin')
-  ),
-  // get all
-  get: () => (
-    query('select * from admin')
-  ),
-  // create a admin
-  insert: (values) => (
-    query(`insert into admin (${C.ROLE.NAME}) values ("${values[C.ROLE.NAME]}")`)
-  ),
-  // deletes a admin by id
-  delete: (id) => (
-    query(`delete from admin where id=${id}`)
-  ),
-  // edits a admin by id
-  edit: ({ id, ...rest }) => (
-    query(`update admin set ${Object.keys(rest).map(x => `${x} = '${rest[x]}'`).join(', ')} where id=${id}`)
-  ),
-};
+const role = buildStore({
+  name: 'role',
+  structure: `
+    ${C.ROLE.ID} int primary key auto_increment,
+    ${C.ROLE.NAME} varchar(255) not null
+  `,
+});
+
+const admin = buildStore({
+  name: 'admin',
+  structure: `
+    ${C.ADMIN.ID} int primary key auto_increment,
+    ${C.ADMIN.FIRST_NAME} varchar(255) not null,
+    ${C.ADMIN.LAST_NAME} varchar(255) not null,
+    ${C.ADMIN.ROLE_ID} int not null,
+    foreign key fk_roleId(${C.ADMIN.ROLE_ID}) references role(${C.ROLE.ID})
+  `,
+});
 
 
 module.exports = {
